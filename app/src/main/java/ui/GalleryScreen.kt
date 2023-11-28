cd887459f6d39969abc623f8800c78ec701a74be
emmanueldebrah2023 <emmanuel.debrah@tui.co.uk>
2023-11-28 14:33:28 +0000

    Revert "Created a GalleryButton to navigate to the GalleryScreen.Also optimized imports and got rid of a TODO."
    This reverts commit acb66474fbf9a9a691120094dce4625c03d344c6.

--- app/src/main/java/ui/GalleryScreen.kt
+++ app/src/main/java/ui/GalleryScreen.kt
@@ -1,20 +1,5 @@
 package ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+class GalleryScreen {
 
-@Composable
-fun GalleryScreen()
-{
-    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-
-        // Creating a Text
-        Text("Hello Geek!", fontSize = 50.sp)
-    }
 }
